b5db80d5933cc9e43408a884e2a1fbec731058d9
tganz <tganz5890@googlemail.com>
2020-10-06 18:46:50 +0200

    add index and show to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,26 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the Thoughts blog #{@user.name}. You have succesfully created a user."
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
   end
 
   private
