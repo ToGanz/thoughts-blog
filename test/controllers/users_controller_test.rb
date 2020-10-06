4f625df33a4ed0f9c307f5d6646a3a7ae72ce3e8
tganz <tganz5890@googlemail.com>
2020-10-06 19:05:50 +0200

    add users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(name: "User1",
+                     email: "user1@test.com",
+                     password: "password", password_confirmation: "password")
+
+    @user2 = User.new(name: "User2",
+                      email: "user2@test.com",
+                      password: "password", password_confirmation: "password")
+  end
+
+  test "should get index" do
+    get users_path
+    assert_response :success
+  end
+  
+  test "should show category" do
+    get category_url(@category)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+
 end
