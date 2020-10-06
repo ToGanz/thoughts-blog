b41281300c48d3d7a680a86d964367d9de75d284
tganz <tganz5890@googlemail.com>
2020-10-06 18:59:43 +0200

    add test for usersmodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   before_save   :downcase_email
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
@@ -16,5 +16,5 @@ class User < ApplicationRecord
   def downcase_email
     self.email.downcase!
   end
-  
+
 end
