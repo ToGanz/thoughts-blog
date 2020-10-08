7e2e73762845edfa74bc76a284bd4424308da88f
tganz <tganz5890@googlemail.com>
2020-10-08 06:28:22 +0200

    login on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      reset_session
+      log_in(@user)
       flash[:notice] = "Welcome to the Thoughts blog #{@user.name}. You have succesfully created a user."
       redirect_to @user
     else
