42fd374f5bfd4373d9d3299d62171b0d3287cdf8
tganz <tganz5890@googlemail.com>
2020-10-17 04:46:42 +0200

    update host for emails for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'thoughts-of-coders.herokuapp.com'
+  host = 'https://thoughts-of-coders.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
