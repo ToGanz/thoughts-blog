800563b126bd763d982d53dd7e9a486797b9374b
tganz <tganz5890@googlemail.com>
2020-10-21 05:31:07 +0200

    add seeds for users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "tobi",
+             email: "test@test.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+
+10.times do |n|
+  name  = Faker::Name.name
+  email = "test-#{n+1}@test.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+# Posts
+users = User.order(:created_at).take(6)
+50.times do
+  title = Faker::Lorem.sentence(word_count: 1)
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.posts.create!(title: title, content: content) }
+end
