fe525b42de101c4d9f44f99090565412257024eb
tganz <tganz5890@googlemail.com>
2020-10-21 06:09:59 +0200

    add posts show and index methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   before_action :require_user, except: [:show, :index]
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def index
+    @posts = Post.paginate(page: params[:page])
   end
 
   def new
