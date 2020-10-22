47b6d2f5a1095471a44299430a6a58815643bc52
tganz <tganz5890@googlemail.com>
2020-10-22 19:12:46 +0200

    add validation  for image upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,10 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { minimum: 2, maximum: 100 }
   validates :content, presence: true, length: { minimum: 10 }
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: "must be a valid image format" },
+                      size:         { less_than: 5.megabytes,
+                                      message:   "should be less than 5MB" }
+  
+                                    
 end
