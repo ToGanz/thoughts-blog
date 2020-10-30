2e93ba3f89d28f7a5fc979180e0890639ca2848f
tganz <tganz5890@googlemail.com>
2020-10-30 05:29:55 +0100

    update fixtures

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
-    @comments = @post.comments
+    @comments = @post.comments.paginate(page: params[:page], per_page: 10)
   end
 
   def index
