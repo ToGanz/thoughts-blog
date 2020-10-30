fa13f5624dc245b8a4800e27bd0ff96ddd9963c6
tganz <tganz5890@googlemail.com>
2020-10-30 05:50:36 +0100

    restrict post creation to admins

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_user, except: [:show, :index]
   before_action :require_correct_user, only: :destroy
+  before_action :require_admin, only: [:new, :create]
 
   def show
     @post = Post.find(params[:id])
