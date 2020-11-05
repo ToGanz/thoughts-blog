413c63360f4d10a7f0d377fd12d48a24d4a76339
tganz <tganz5890@googlemail.com>
2020-11-05 04:20:40 +0100

    add styling

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -9,7 +9,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   test "profile display" do
     get user_path(@user)
     assert_template 'users/show'
-    assert_select 'h2', text: @user.name
+    assert_select 'h2', text: "Name: " + @user.name
     assert_match @user.posts.count.to_s, response.body
     assert_select 'div.pagination'
     @user.posts.paginate(page: 1).each do |post|
